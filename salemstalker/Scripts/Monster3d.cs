ebb39da1bc792cc14d86c45ec3c29b0ee4032d2d
alexPrice2123 <alexander.price2123@gmail.com>
2025-09-02 14:53:55 -0600

    Monster that follows you and a sword that doesnt do anything

--- salemstalker/Scripts/Monster3d.cs
+++ salemstalker/Scripts/Monster3d.cs
@@ -3,24 +3,39 @@ using System;
 
 public partial class Monster3d : CharacterBody3D
 {
-	public const float Speed = 10.0f; //Player speed
+	public const float Speed = 5.0f; //Player speed
 	public const float JumpVelocity = 6.5f; //Jump power
+
+	private Player3d _player;
+
 
 	public override void _Ready()
 	{
-		//
+		_player = this.GetParent().GetNode<Player3d>("Player_3d");
+		StandardMaterial3D material = GetNode<MeshInstance3D>("MeshInstance3D").MaterialOverlay as StandardMaterial3D;
+		GetNode<MeshInstance3D>("MeshInstance3D").SetSurfaceOverrideMaterial(0, material);
+		material.Transparency = BaseMaterial3D.TransparencyEnum.Alpha;
+		material.EmissionEnabled = true;
+		Color basecolor = material.AlbedoColor;
+		basecolor.A = 1.0f;
+		material.AlbedoColor = basecolor;
 	}
 
 	public override void _PhysicsProcess(double delta) //Event tick; happens every frame
 	{
-		Vector3 velocity = Velocity;
+
+		Vector3 direction = (_player.GlobalPosition - GlobalPosition).Normalized();
+
+		direction.Y = 0;
+
+		
 
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
-			velocity += GetGravity() * (float)delta;
+			direction += GetGravity() * (float)delta;
 		}
-		Velocity = velocity;
+		Velocity = direction * Speed;
 		MoveAndSlide();
 	}
 }
