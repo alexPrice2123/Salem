9051c8336b3a829934f70610abbb2b6bc49963a4
alexPrice2123 <alexander.price2123@gmail.com>
2025-09-04 14:34:38 -0600

    low poly rat
    low poly rat

--- salemstalker/Scripts/NpcVillager.cs
+++ salemstalker/Scripts/NpcVillager.cs
@@ -3,10 +3,10 @@ using System;
 
 public partial class NpcVillager : CharacterBody3D
 {
-    public const float speed = 5.0f ;
+	public const float speed = 5.0f;
 
 
-    public override void _PhysicsProcess(double delta) //Event tick; happens every frame
+	public override void _PhysicsProcess(double delta) //Event tick; happens every frame
 	{
 		Vector3 velocity = Velocity;
 
@@ -17,5 +17,13 @@ public partial class NpcVillager : CharacterBody3D
 		}
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+	
+	private void _on_hitbox_body_entered(Node3D body)
+	{
+		if (body.IsInGroup("Monster"))
+		{
+			
+		}
 	}
 }
