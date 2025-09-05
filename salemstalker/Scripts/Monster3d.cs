edf04b64b328a16f1ec5f4c22733e091fc51ae7a
alexPrice2123 <alexander.price2123@gmail.com>
2025-09-05 14:47:45 -0600

    Pathfinding Monster
    also added wandering to the monster if you get too far away

--- salemstalker/Scripts/Monster3d.cs
+++ salemstalker/Scripts/Monster3d.cs
@@ -7,12 +7,17 @@ public partial class Monster3d : CharacterBody3D
 	public const float Speed = 5.0f; //Player speed
 	public const float JumpVelocity = 6.5f; //Jump power
 	public const float MaxHealth = 5.0f; //Max Health
+	public const float Range = 25.0f; //Range where it can detect the player
 
 	private Player3d _player;
 	private StandardMaterial3D _material;
 	public float _health = MaxHealth;
     private Vector3 _knockbackVelocity = Vector3.Zero;
-
+	private Vector3 _wanderPos;
+	private float _count;
+	private RandomNumberGenerator _rng = new RandomNumberGenerator();
+	private NavigationAgent3D _navAgent;
+	private Vector3 startPos;
 
 	public override void _Ready()
 	{
@@ -24,6 +29,9 @@ public partial class Monster3d : CharacterBody3D
 		Color basecolor = _material.AlbedoColor;
 		basecolor.A = 0.0f;
 		_material.AlbedoColor = basecolor;
+		_rng.Randomize();
+		_navAgent = GetNode<NavigationAgent3D>("NavigationAgent3D");
+		startPos = GlobalPosition;
 	}
 
 	private async void _on_hitbox_area_entered(Area3D body)
@@ -39,6 +47,9 @@ public partial class Monster3d : CharacterBody3D
 			_material.AlbedoColor = basecolor;
 			await ToSignal(GetTree().CreateTimer(0.1f), "timeout");
 			basecolor.A = 0.0f;
+			float randZ = GlobalPosition.Z + _rng.RandiRange(-50, 50);
+			float randX = GlobalPosition.X + _rng.RandiRange(-50, 50);
+			_wanderPos = new Vector3(randX, 0f, randZ);
 			_material.AlbedoColor = basecolor;
 			_health -= _damage;
 			if (_health <= 0){
@@ -49,24 +60,33 @@ public partial class Monster3d : CharacterBody3D
 
 	public override void _PhysicsProcess(double delta) //Event tick; happens every frame
 	{
-
-		Vector3 direction = (_player.GlobalPosition - GlobalPosition).Normalized();
-
-		direction.Y = 0;
-		Vector3 playerPos = _player.GlobalPosition;
-		LookAt(new Vector3(playerPos.X, GlobalPosition.Y, playerPos.Z), Vector3.Up);
-
-		GD.Print(_knockbackVelocity);
-		// Add the gravity.
-		if (!IsOnFloor())
+		_count += 1;
+		if (_count >= 250)
 		{
-			direction += GetGravity() * (float)delta;
+			_count = _rng.RandiRange(-100, 50);
+			float randZ = startPos.Z + _rng.RandiRange(-50, 50);
+			float randX = startPos.X + _rng.RandiRange(-50, 50);
+			_wanderPos = new Vector3(randX, 0f, randZ);
 		}
-		Velocity = (direction * Speed)+_knockbackVelocity;
+		float distance = (_player.GlobalPosition - GlobalPosition).Length();
+		if (distance <= Range)
+		{
+			_navAgent.TargetPosition = _player.GlobalPosition;
+			Vector3 nextPoint = _navAgent.GetNextPathPosition();
+			Velocity = ((nextPoint - GlobalTransform.Origin).Normalized() * Speed)+_knockbackVelocity;
+			Vector3 playerPos = _player.GlobalPosition;
+			LookAt(new Vector3(playerPos.X, GlobalPosition.Y, playerPos.Z), Vector3.Up);
+		}
+		else
+		{
+			_navAgent.TargetPosition = _wanderPos;
+			Vector3 nextPoint = _navAgent.GetNextPathPosition();
+			Velocity = ((nextPoint - GlobalTransform.Origin).Normalized() * Speed)+_knockbackVelocity;
+			LookAt(new Vector3(_wanderPos.X, GlobalPosition.Y, _wanderPos.Z), Vector3.Up);
+		}
 		MoveAndSlide();
 		_knockbackVelocity = _knockbackVelocity.Lerp(Vector3.Zero, (float)delta * 5.0f);
 	}
 }
-	//Color basecolor = _material.AlbedoColor;
-	//	basecolor.A = 1.0f;
-	//	_material.AlbedoColor = basecolor;
+
+
