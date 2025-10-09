69f99f5f5552ff47b493e7f495e3ccfd83287e9b
alexPrice2123 <alexander.price2123@gmail.com>
2025-10-09 13:25:09 -0600

    Cultist deals damage now

--- salemstalker/Scripts/Orb.cs
+++ salemstalker/Scripts/Orb.cs
@@ -4,13 +4,27 @@ using System;
 public partial class Orb : RigidBody3D
 {
 	// Called when the node enters the scene tree for the first time.
-	public void Shoot(Vector3 direction)
+	public Player3d _playerOrb;
+	public float _damageOrb;
+	private CollisionShape3D _attackBox;
+	public void Shoot(float speed)
 	{
-		ApplyCentralImpulse(direction * 5f);
+		_attackBox = GetNode<CollisionShape3D>("Area3D/CollisionShape3D");
+		LookAt(new Vector3(_playerOrb.GlobalPosition.X, GlobalPosition.Y, _playerOrb.GlobalPosition.Z), Vector3.Up);
+		ApplyCentralImpulse(-GlobalTransform.Basis.Z.Normalized() * speed);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void _on_attackbox_area_entered(Node3D body)
+	{
+		if (body.IsInGroup("Player") && body.Name == "Hurtbox")
+		{
+			_playerOrb._health -= _damageOrb;
+			_attackBox.Disabled = true;
+		}
 	}
 }
--- salemstalker/Scripts/VCultist.cs
+++ salemstalker/Scripts/VCultist.cs
@@ -7,6 +7,7 @@ public partial class VCultist : Monster3d
 
     private PackedScene _darkOrb = GD.Load<PackedScene>("res://Scenes/Monsters/MonsterAssets/orb.tscn"); // Scene reference to the dark orb
     private Node3D _spawn;
+    private float _projectileSpeed = 15f;
     public override void _Ready()
     {
         Speed = 4.5f;             // Movement speed
@@ -16,7 +17,7 @@ public partial class VCultist : Monster3d
         BaseDamage = 15.0f;
         WanderRange = 50;
         AttackSpeed = 6f;
-        AttackRange = 15f;
+        AttackRange = 1f;
         Monster = this;
 
         _spawn = GetNode<Node3D>("Body/metarig/Skeleton3D/Cylinder/Spawn");
@@ -42,18 +43,23 @@ public partial class VCultist : Monster3d
             _hasHit = true;
         }
     }
-    
+
     public async void Attack()
-	{
+    {
         _canAttack = false;
+        _attackAnim = true;
         await ToSignal(GetTree().CreateTimer(AttackSpeed), "timeout");
-		RigidBody3D projectileInstance = _darkOrb.Instantiate<RigidBody3D>(); // Create monster instance
+        RigidBody3D projectileInstance = _darkOrb.Instantiate<RigidBody3D>(); // Create monster instance
         _player.GetParent().AddChild(projectileInstance);                                             // Add monster to holder node
         projectileInstance.GlobalPosition = _spawn.GlobalPosition;
         if (projectileInstance is Orb orb)
         {
-            orb.Shoot(-GlobalTransform.Basis.Z.Normalized());
-        }  
+            orb._playerOrb = _player;
+            orb._damageOrb = BaseDamage + _damageOffset;
+            orb.Shoot(_projectileSpeed);
+        }
         _canAttack = true;
+        await ToSignal(GetTree().CreateTimer(0.5), "timeout");
+        _attackAnim = false;
 	}
 }
