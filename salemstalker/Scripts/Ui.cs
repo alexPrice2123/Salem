167aa0ab542ca88874333422340adf235060f164
Gamedagamez <gamedagames@gmail.com>
2025-10-21 14:32:46 -0600

    Secondary weapon slot selector!

--- salemstalker/Scripts/Ui.cs
+++ salemstalker/Scripts/Ui.cs
@@ -7,9 +7,11 @@ public partial class Ui : Control
 {
 	private Player3d _player;
 	private string _hovering = "falchionHover";
+	private string _secItemSend = null;
 	private bool _loaded = false;
 	private Control _loadingUI;
 	private ShaderMaterial _loadingMaterial;
+	private Control _slotSelect;
 	public override void _Ready()
 	{
 		if (GetParent() is Player3d player)
@@ -17,6 +19,7 @@ public partial class Ui : Control
 			_player = player;
 		}
 		_loadingUI = GetNode<Control>("Loading");
+		_slotSelect = GetNode<Control>("Inv/SubPort/Sub/SlotSelector");
 		_loadingUI.Visible = true;
 		_loadingMaterial = _loadingUI.Material as ShaderMaterial;
 		Load();
@@ -45,18 +48,32 @@ public partial class Ui : Control
 
 
 	// --- Falchion ---
-	private void _on_falchion_mouse_entered(){ PlayAnim("Falchion", true); }
-	private void _on_falchion_mouse_exited(){ PlayAnim("Falchion", false); }
+	private void _on_falchion_mouse_entered() { PlayAnim("Falchion", true); }
+	private void _on_falchion_mouse_exited() { PlayAnim("Falchion", false); }
 	private void _on_falchion_button_up() { _player.SwitchPrimaryWeapon("Falchion"); }
 	
 	// --- ShortSword ---
-	private void _on_shortsword_mouse_entered(){ PlayAnim("ShortSword", true); }
-	private void _on_shortsword_mouse_exited(){ PlayAnim("ShortSword", false); }
+	private void _on_shortsword_mouse_entered() { PlayAnim("ShortSword", true); }
+	private void _on_shortsword_mouse_exited() { PlayAnim("ShortSword", false); }
 	private void _on_shortsword_button_up() { _player.SwitchPrimaryWeapon("ShortSword"); }
 
 	// --- StakeGun ---
-	private void _on_stake_gun_mouse_entered(){ PlayAnim("StakeGun", true); }
-	private void _on_stake_gun_mouse_exited(){ PlayAnim("StakeGun", false); }
+	private void _on_stake_gun_mouse_entered() { PlayAnim("StakeGun", true); }
+	private void _on_stake_gun_mouse_exited() { PlayAnim("StakeGun", false); }
+	private void _on_stake_gun_button_up()
+	{
+		_secItemSend = "StakeGun";
+		_slotSelect.Visible = true;
+	}
+
+	// --- Flintlock ---
+	private void _on_gun_mouse_entered() { PlayAnim("flintlock", true); }
+	private void _on_gun_mouse_exited() { PlayAnim("flintlock", false); }
+	private void _on_gun_button_up()
+	{
+		_secItemSend = "FlintGun";
+		_slotSelect.Visible = true;
+    }
 
 	private async void Load()
 	{
@@ -77,11 +94,39 @@ public partial class Ui : Control
 		{
 			GetNode<Node3D>("Inv/SubPort/Sub/InvWorld").GetNode<Node3D>(sword).GetNode<AnimationPlayer>("HoverAnim").Play("Hover");
 		}
-        else
-        {
-        	GetNode<Node3D>("Inv/SubPort/Sub/InvWorld").GetNode<Node3D>(sword).GetNode<AnimationPlayer>("HoverAnim").PlayBackwards("Hover");
-        }
+		else
+		{
+			GetNode<Node3D>("Inv/SubPort/Sub/InvWorld").GetNode<Node3D>(sword).GetNode<AnimationPlayer>("HoverAnim").PlayBackwards("Hover");
+		}
 
 	}
+
+	private void _on_slot_4_button_up()
+	{
+		_slotSelect.Visible = false;
+		GD.Print("sendslot4");
+		_secItemSend = null;
+	}
+
+	private void _on_slot_3_button_up()
+	{
+		_slotSelect.Visible = false;
+		GD.Print("sendslot3");
+		_secItemSend = null;
+	}
+
+	private void _on_slot_2_button_up()
+	{
+		_slotSelect.Visible = false;
+		GD.Print("sendslot2");
+		_secItemSend = null;
+	}
+	
+	private void _on_slot_1_button_up()
+    {
+		_slotSelect.Visible = false;
+		GD.Print("sendslot1");
+		_secItemSend = null;
+    }
 	
 }
