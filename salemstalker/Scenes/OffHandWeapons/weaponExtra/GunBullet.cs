aa8a9dcd58a9f66890189fc51bd53aecfa131880
Gamedagamez <gamedagames@gmail.com>
2025-10-28 14:34:49 -0600

    Gun changes!

--- salemstalker/Scenes/OffHandWeapons/weaponExtra/GunBullet.cs
+++ salemstalker/Scenes/OffHandWeapons/weaponExtra/GunBullet.cs
@@ -1,25 +1,27 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 
 public partial class GunBullet : RigidBody3D
 {
-    private float _speed = 50.0f;
+    private float _speed = 10.0f;
     private int _count = 0;
     public override void _PhysicsProcess(double delta)
     {
         ApplyCentralImpulse(GlobalTransform.Basis.Z.Normalized() * _speed);
         _count += 1;
-        if (_count > 250)
+        if (_count > 500)
         {
+            GD.Print("bulletDespawn");
             QueueFree();
         }
     }
     
-    public void _on_hit_area_3d_body_entered(Node3D body)
+    public async void _on_hit_area_3d_body_entered(Node3D body)
     {
         if (body.IsInGroup("Monster"))
         {
-            GD.Print("HIIHIHIIHH");
+            GD.Print("munchHit");
             QueueFree();
         }
     }
