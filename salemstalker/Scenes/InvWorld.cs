0de55eb4a70c8f76a05e2422c31a4571d85eeacb
CherriHoney <tshuki67@gmail.com>
2025-11-07 14:48:28 -0700

    stop touching the tscns im in ALEX (texture fix)

--- salemstalker/Scenes/InvWorld.cs
+++ salemstalker/Scenes/InvWorld.cs
@@ -4,16 +4,16 @@ using System;
 public partial class InvWorld : Node3D
 {
 
-    private async void _on_falchion_box_mouse_entered()
-    {
-        GetNode<AnimationPlayer>("AnimationPlayer").Play("falchionHover");
-        await ToSignal(GetTree().CreateTimer(0.1f), "timeout");
-        GD.Print("Testing?");
-        
-    }
-    private async void _on_falchion_box_mouse_exited()
-    {
-        GetNode<AnimationPlayer>("AnimationPlayer").Play("falchionDehover");
-        await ToSignal(GetTree().CreateTimer(0.2f), "timeout");
-    }
+	private async void _on_falchion_box_mouse_entered()
+	{
+		GetNode<AnimationPlayer>("AnimationPlayer").Play("falchionHover");
+		await ToSignal(GetTree().CreateTimer(0.1f), "timeout");
+		GD.Print("Testing?");
+		
+	}
+	private async void _on_falchion_box_mouse_exited()
+	{
+		GetNode<AnimationPlayer>("AnimationPlayer").Play("falchionDehover");
+		await ToSignal(GetTree().CreateTimer(0.2f), "timeout");
+	}
 }
