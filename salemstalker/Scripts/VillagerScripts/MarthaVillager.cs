87b8419b234f71e06287ff08e4606938b15c2259
DqrkTimes <lukinhomota@gmail.com>
2025-12-01 12:40:48 -0700

    Fixed Martha and Dillion Dialogue

--- salemstalker/Scripts/VillagerScripts/MarthaVillager.cs
+++ salemstalker/Scripts/VillagerScripts/MarthaVillager.cs
@@ -12,7 +12,7 @@ public partial class MarthaVillager /*Replace with script name*/ : NpcVillager
 	public override void _PhysicsProcess(double delta)
 	{
 		EveryFrame(delta);
-		if (_player._swampMonstersKilled >= 5 && _player._plainsMonstersKilled >=5 && _player._forestMonstersKilled >= 5) //Replace with the correct quest completion condition
+		if (_player._forestMonstersKilled >= 5) //Replace with the correct quest completion condition
 		{
 			_questComplete = true;
 		}
