3d3c1adf4b3304ceb5e69f2683014d8eb66e74cf
alexPrice2123 <alexander.price2123@gmail.com>
2026-01-29 13:14:38 -0700

    Fixed Merge Issue

--- salemstalker/Scripts/PauseMenu.cs
+++ salemstalker/Scripts/PauseMenu.cs
@@ -1,17 +1,12 @@
 using Godot;
 using System;
-using System.Collections.Generic;
 
 public partial class PauseMenu : Control
 {
 	// Called when the node enters the scene tree for the first time.
 	public Player3d _player;
-	private Dictionary<int,DisplayServer.WindowMode> windowMode;
 	public override void _Ready()
 	{
-		windowMode.Add(0,DisplayServer.WindowMode.Windowed);
-		windowMode.Add(1,DisplayServer.WindowMode.Fullscreen);
-		windowMode.Add(2,DisplayServer.WindowMode.ExclusiveFullscreen);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,9 +26,4 @@ public partial class PauseMenu : Control
 		QueueFree();
 		_player.UnPause();
     }
-
-	private void _on_windowMode_up()
-	{
-		DisplayServer.WindowSetMode(windowMode[GetNode<OptionButton>("OptionButton").Selected]);
-	}
 }
