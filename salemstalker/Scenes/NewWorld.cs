1175857a65ade4de31b5d0dcd0e2c505e82773e6
Gamedagamez <gamedagames@gmail.com>
2026-03-10 14:00:12 -0600

    I AM SO TWEAK

--- salemstalker/Scenes/NewWorld.cs
+++ salemstalker/Scenes/NewWorld.cs
@@ -1,6 +1,41 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class NewWorld : Node3D
 {
+    private Godot.Collections.Dictionary<string,Variant> data ;
+    string savePath = "user://saveData.json";
+    // Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+    {
+        if (!FileAccess.FileExists(savePath)){data.Add("place","testing");SaveToFile(data);}
+        GD.Print( " this is a testing yeah1");
+        data = LoadFromFile();
+        GD.Print( " this is a testing yeah5");
+        GD.Print(data["place"], " this is a testing yeah");
+        GD.Print( " this is a testing yeah6");
+    }
+
+    public void SaveToFile(Godot.Collections.Dictionary<string,Variant> toSave)
+    {
+        GD.Print( " this is a testing yeah5");
+        using FileAccess file = FileAccess.Open(savePath, FileAccess.ModeFlags.Write);
+        GD.Print( " this is a testing yeah5");
+        string jsonData = Json.Stringify(toSave);
+        GD.Print( " this is a testing yeah5");
+        file.StoreLine(jsonData);
+        GD.Print( " this is a testing yeah5");
+    }
+
+    public Godot.Collections.Dictionary<string,Variant> LoadFromFile()
+    {
+        GD.Print( " this is a testing yeah2");
+        using FileAccess file = FileAccess.Open(savePath, FileAccess.ModeFlags.Read);
+        GD.Print( " this is a testing yeah3");
+        string content = file.GetAsText(); 
+        GD.Print( " this is a testing yeah4");
+        return (Godot.Collections.Dictionary<string,Variant>)Json.ParseString(content);
+        
+    }
 }
