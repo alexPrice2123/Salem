d06b903ab7250c28cea1cb9d25c44b33734caf2c
Gamedagamez <gamedagames@gmail.com>
2026-03-11 14:19:42 -0600

    IT WORKS

--- salemstalker/Scenes/NewWorld.cs
+++ salemstalker/Scenes/NewWorld.cs
@@ -1,38 +1,28 @@
 using Godot;
-using System;
+using System; 
 using System.Collections.Generic;
 
 public partial class NewWorld : Node3D
 {
-    private Godot.Collections.Dictionary<string,Variant> data = new Godot.Collections.Dictionary<string,Variant>();
-    string savePath = "user://saveData.json";
+    public Godot.Collections.Dictionary<string,Variant> data = new Godot.Collections.Dictionary<string,Variant>();
+    private string _savePath = "user://saveData.json";
+
     // Called when the node enters the scene tree for the first time.
 	public override void _Ready()
     {
-        if (!FileAccess.FileExists(savePath)){data.Add("place","testing");SaveToFile(data);GD.Print("Save Data created!");}
-        data = LoadFromFile();
-        GD.Print(data.ToString());
+        if ( !FileAccess.FileExists(_savePath) )
+        {
+            GD.Print("Save file missing");
+            SaveHandler.createSaveFile(_savePath);
+            GD.Print("Save file created");
+        }
+        else{ GD.Print("Save file exists"); }
+        data = SaveHandler.LoadFromFile(_savePath);
+        GD.Print("Save file loaded");
+        if ((bool)data["tutorialComplete"] == true)
+        {
+            GetNode<CharacterBody3D>("Player_3d").GlobalPosition = GetNode<Marker3D>("VillageMark").GlobalPosition ;
+        }
     }
-
-    public void SaveToFile(Godot.Collections.Dictionary<string,Variant> toSave)
-    {
-        GD.Print( " this is a testing yeah5");
-        using FileAccess file = FileAccess.Open(savePath, FileAccess.ModeFlags.Write);
-        GD.Print( " this is a testing yeah5");
-        string jsonData = Json.Stringify(toSave);
-        GD.Print( " this is a testing yeah5");
-        file.StoreLine(jsonData);
-        GD.Print( " this is a testing yeah5");
-    }
-
-    public Godot.Collections.Dictionary<string,Variant> LoadFromFile()
-    {
-        GD.Print( " this is a testing yeah2");
-        using FileAccess file = FileAccess.Open(savePath, FileAccess.ModeFlags.Read);
-        GD.Print( " this is a testing yeah3");
-        string content = file.GetAsText(); 
-        GD.Print( " this is a testing yeah4");
-        return (Godot.Collections.Dictionary<string,Variant>)Json.ParseString(content);
-        
-    }
+    
 }
