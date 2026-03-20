7851b8faf181396c08160d35af456445d6856f25
alexPrice2123 <alexander.price2123@gmail.com>
2026-03-20 12:12:09 -0600

    cats look at you

--- salemstalker/Scripts/Object.cs
+++ salemstalker/Scripts/Object.cs
@@ -7,6 +7,7 @@ public partial class Object : CharacterBody3D
 	private RandomNumberGenerator _rng = new RandomNumberGenerator();
 	[Export]
 	bool _applyGravity = false;
+	MeshInstance3D _currentObject;
 
 	public async override void _Ready()
 	{
@@ -14,7 +15,8 @@ public partial class Object : CharacterBody3D
 		GD.Print(Name);	
 		_rng.Randomize();
 	   	if (((string)Name).Contains("Log")){GetNode<Node3D>($"Log{_rng.RandiRange(1,2)}").Visible = true;}
-		else {GetNodeOrNull<MeshInstance3D>((string)Name).Visible = true;}
+		else {_currentObject = GetNodeOrNull<MeshInstance3D>((string)Name); _currentObject.Visible = true;}
+		
 	}
 	public override void _PhysicsProcess(double delta)
 	{
@@ -24,10 +26,30 @@ public partial class Object : CharacterBody3D
 			MoveAndSlide();
 		}
 		if (_player == null){return;}
+		if (Name == "Taz" || Name == "Bridger" || Name == "Rogue" || Name == "Gnocchi")
+        {
+            GetNode<Node3D>("GoalLookAt").LookAt(new Vector3(_player.GlobalPosition.X, GlobalPosition.Y, _player.GlobalPosition.Z), Vector3.Up);
+			RotateFunc(delta, GetNode<Node3D>("GoalLookAt"));
+        }
+		
+
 		if (_player._lastSeen != this)
 		{
 			GetNode<Label3D>("Title").Visible = false;
 		}
 		
+	}
+
+	private void RotateFunc(double delta, Node3D lookDirection)
+	{
+		if (Mathf.RadToDeg(lookDirection.GlobalRotation.Y) >= 175 || Mathf.RadToDeg(lookDirection.GlobalRotation.Y) <= -175)
+		{
+			GlobalRotation = new Vector3(GlobalRotation.X, lookDirection.GlobalRotation.Y, GlobalRotation.Z);
+		}
+		else
+		{
+			float newRotation = Mathf.Lerp(GlobalRotation.Y, lookDirection.GlobalRotation.Y, (float)delta * 10f);
+			GlobalRotation = new Vector3(GlobalRotation.X, newRotation, GlobalRotation.Z);
+		}
 	}
 }
