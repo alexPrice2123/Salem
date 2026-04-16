557c9b839f8b40f22dffe2ec02f5c19732e40e03
Gamedagamez <gamedagames@gmail.com>
2026-04-16 14:45:01 -0600

    ok i push

--- salemstalker/Scenes/OffHandWeapons/Caltrops.cs
+++ salemstalker/Scenes/OffHandWeapons/Caltrops.cs
@@ -18,13 +18,13 @@ public partial class Caltrops : Node3D
         await ToSignal(GetTree().CreateTimer(1.45f), "timeout");
         RandomNumberGenerator _rng = new();   
         GD.Print("workplease");
-        for(int i = 0; i < 50; i++)
+        for(int i = 0; i < 5; i++)
         {
             GD.Print("workplease2");
             RigidBody3D temPrj = _gunBullet.Instantiate<RigidBody3D>();
             AddSibling(temPrj);
             temPrj.Position = _barrel.Position;  
-            temPrj.Rotation = _barrel.Rotation + new Vector3(0,_rng.RandfRange(0.1f,130f),0f);
+            temPrj.Rotation = _barrel.Rotation + new Vector3(0,Mathf.DegToRad(_rng.RandfRange(-60f,60f)),0f);
             temPrj.Reparent(temPrj.GetParent().GetParent().GetParent().GetParent().GetParent());
             GD.Print("workplease3");
         }
