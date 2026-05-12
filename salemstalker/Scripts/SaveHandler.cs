b7919481beaf680c25cd43c282a75a78659bb217
Gamedagamez <gamedagames@gmail.com>
2026-05-12 14:51:49 -0600

    killing alexandr

--- salemstalker/Scripts/SaveHandler.cs
+++ salemstalker/Scripts/SaveHandler.cs
@@ -63,7 +63,7 @@ public partial class SaveHandler : GodotObject
         else{GD.Print("File exists, reseting");}
         using FileAccess file = FileAccess.Open(savePath, FileAccess.ModeFlags.Write);
         string jsonData = Json.Stringify(new Godot.Collections.Dictionary<string, Variant>{
-            { "version", "0.6.1a"},
+            { "version", "0.6.1b"},
             { "lastLocation", "intro" },
             { "tutorialComplete", false},
             { "mainWeaponUnlocked", new Godot.Collections.Dictionary<string, int>{
@@ -167,7 +167,7 @@ public partial class SaveHandler : GodotObject
     {
         Godot.Collections.Dictionary<string,Variant> data = LoadFromFile(savePath);
         if(!data.ContainsKey("version")) {GD.Print("Save is not up to date!"); return(false);}
-        if (((string)data["version"]).Equals("0.6.1a")){GD.Print("Save is up to date!"); return(true);}
+        if (((string)data["version"]).Equals("0.6.1b")){GD.Print("Save is up to date!"); return(true);}
         else{GD.Print("Save is not up to date!"); return(false);}
     }
 }
